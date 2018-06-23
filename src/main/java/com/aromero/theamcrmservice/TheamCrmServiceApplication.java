8d173b7cdc869ebc84a76c0d10aa3e40816f964d
Kalm004 <kalm004@gmail.com>
2018-06-23 11:56:26 +0100

    Empty SprintBoot project.

---
+++ src/main/java/com/aromero/theamcrmservice/TheamCrmServiceApplication.java
@@ -0,0 +1,12 @@
+package com.aromero.theamcrmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheamCrmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheamCrmServiceApplication.class, args);
+    }
+}
